0dfcdfee98e21ccb53dd4e0340e9cd295d378249
Diego <df.rojas.marcial@gmail.com>
2019-05-23 15:11:40 -0500

    update info

--- src/en/js/team.js
+++ src/en/js/team.js
@@ -1,14 +1,14 @@
 $("#teamInfo").hide();
 
 var namesTeam = [
-    "<b>Carlos Michel</b><br>      CEO y fundador",
-    "<b>Verónica Pastrana</b><br>  Directora de Proyectos",
-    "<b>Pawel Lewinski</b><br>     Tecnología y Sistemas",
-    "<b>Humberto Zugasti</b><br>   Contenido educativo y arte",
-    "<b>Alejandra Morales</b><br>  Comunicación y arte",
-    "<b>Julia Ivalú</b><br>        Educación y contenido",
+    "<b>Carlos Michel</b><br>      CEO and founder",
+    "<b>Verónica Pastrana</b><br>  Project Manager",
+    "<b>Pawel Lewinski</b><br>     Technology and Systems",
+    "<b>Humberto Zugasti</b><br>   Educational content and art",
+    "<b>Alejandra Morales</b><br>  Communication and Art",
+    "<b>Julia Ivalú</b><br>        Education and content",
     "<b>Diego Rojas</b><br>        UX/UI Frontend",
-    "<b>Ignacio Ordoñez</b><br>    Contabilidad y finanzas"
+    "<b>Ignacio Ordoñez</b><br>    Accounting and Finance"
 
 ]
 
@@ -25,63 +25,119 @@ var photosTeam=[
 ];
 
 var bioTeam = [
-    `Carlos es originario de “El llano en llamas” en Jalisco, México, y su libro de cabecera es “Pedro Páramo”. Tiene más de 15 años de experiencia en Private Equity y Venture Capital. Es
-también CEO de Kurowi Games, empresa de Ed-Tech dedicada a desarrollar contenido
-educativo digital y juegos de video. Es mentor de emprendedores sociales para programas
-como Momentum de BBVA, y New Ventures Exponential de Fomento Social Banamex.
-Carlos estudió Administración Financiera en el TEC de Monterrey, y actualmente trata de
-“aprender a aprender” de los grandes: Charlie Munger, Nassim Taleb, Richard Feynman, Bertrand Russell, Naval Ravikant, H.G. Wells, Borges...`,
-    `<i>“La educación es el arma más poderosa que puedes usar para cambiar al mundo”</i>
+    `Carlos was born at the heart of "The Burning Plain" in Jalisco, México; and his
+    bedside book is "Pedro Páramo". He has more than 15 years of experience in
+    Private Equity and Venture Capital. He is also CEO of Kurowi Games, an Ed-Tech
+    company dedicated to develop digital educational content, and video games. He is
+    a mentor of social entrepreneurs for programs such as Momentum of BBVA, and
+    New Ventures Exponential for Fomento Social Banamex. Carlos studied Financial
+    Administration at the TEC of Monterrey, and is currently trying to "learn how to
+    learn" from the great ones: Charlie Munger, Nassim Taleb, Richard Feynman,
+    Bertrand Russell, Naval Ravikant, H.G. Wells, Borges ...`,
+    `<i>"Education is the most powerful weapon you can use to change the world"</i>
     <br>- Nelson Mandela
 <br>
 <br>
-Ciudad de México, 1990. Nacida en luna llena. Filántropa, viajera, lectora y abogada. Disfruta del
-deporte, de las aventuras y las actividades rodeada de amigos y naturaleza. Procura aprender cosas
-nuevas para cambiar los ojos con los que ve el mundo. Mente abierta y amante de los animales.
-Egresada de la Escuela Libre de Derecho, abogada con experiencia en derecho penal, corporativo,
-litigio constitucional, competencia económica y telecomunicaciones. Apasionada de la educación,
-ama su trabajo y está decidida a ser una más de todas las personas que tenemos que trabajar para
-que la educación en México y Latinoamérica funcione. Como directora de operaciones en The Pale
-Blue Dot tiene experiencia en la implementación de proyectos educativos con tecnología en
-comunidades mexicanas, piensa que hay mucho trabajo por delante y que solo a través de la
-educación y del pensamiento colectivo podrá la humanidad aprender a respetar, amar y cuidar el
-bello planeta al que llamamos “hogar”.`,
-    "Pawel es programador fullstack y desarrollador de videojuegos. Trabajó 4 años en Yogome como desarrollador de videojuegos, desarrollador web y como lider de programación en el área de HTML5, en donde ayudó a coordinar, asesorar, capacitar y desarrollar software en conjunto con un grupo de 10 personas. Junto con el equipo, Pawel fue parte del desarrollo de alrededor de 200 minijuegos, un torneo multijugador de matemáticas usando distintos dispositivos a la vez, libros interactivos educativos, una sección de videos, sección de aventuras con batallas, la plataforma del torneo reto cuantrix para Televisa, entre muchos otros proyectos... Pawel le fascina la tecnología y siempre le gusta estar aprendiendo cosas nuevas, su mayor pasión son los videojuegos tanto jugar como crear.",
-    `<i>“En este lugar perdemos demasiado tiempo mirando hacia atrás.<br>
-Camina hacia el futuro, abriendo nuevas puertas y probando cosas nuevas, se curioso… porque
-nuestra curiosidad siempre nos conduce por nuevos caminos.”</i> <br>
+Mexico City, 1990. Born on a full moon. Philanthropist, traveler, reader and lawyer.
+Enjoy sports, adventures and activities surrounded by friends and nature. She tries
+to learn new things to change the eyes with those she sees the world. Open mind
+and animal lover. Graduated from the Escuela Libre de Derecho, lawyer with
+experience in criminal law, corporate, constitutional litigation, economic competition
+and telecommunications. Passionate about education, she loves her job and she´s
+determined to be one of all the people we have to work to make education in
+México and Latin America work. As director of operations at The Pale Blue Dot she
+has experience in the implementation of educational projects with technology in
+Mexican communities, she thinks that there is a lot of work ahead and that only
+through education and collective thinking can humanity learn to respect, love and
+take care of this beautiful planet we call "home".`,
+
+
+    `Pawel is a full-stack programmer and video game developer. He worked for 4
+    years in Yogome as a video game developer, web developer and as a
+    programming leader in the HTML5 area, where he helped coordinate, advise, train
+    
+    and develop software together with a group of 10 people. Along with the team,
+    Pawel was part of the development of around 200 mini-games, a multiplayer math
+    tournament using different devices at the same time, interactive educational books,
+    a video section, adventure section with battles, the questrix challenge platform for
+    Televisa, among many other projects ... Pawel is fascinated by technology and
+    always likes to be learning new things, his biggest passion is videogames both play
+    and create them.`,
+
+    `<i>"In this place we waste too much time looking back.
+Walk to the future, opening new doors and trying new things, be curious ...
+because our curiosity always leads us in new ways."</i> <br>
 -Walt Disney
 <br>
 <br>
-Julio 17, 1990. Ilustrador y Diseñador Mexicano. Nacido y criado en una ciudad en medio
-del desierto. Le gusta: aprender toda clase de cosas innecesarios. Odia: La frase
-<i>"¡Compórtate como un adulto, por el amor de Dios!"</i>. Tiene experiencia en diseño Web,
-UI/UX y producción para medios. En su último trabajo se desempeñó como líder de
-ilustración para Yogome, una compañía nacional de video juegos educativos. Se sientecómodo trabajando en equipo, y conociendo más de la gente que lo rodea. Es apasionado cuando se trata de aprender sobre otras culturas, idiomas y estudios de género. Ama su trabajo como profesor de medio tiempo en una universidad y a su perro llamado Bob.`,
-    `<i>“El objetivo del arte no es representar la apariencia externa de las cosas, sino su significado interior”</i><br>
--Aristóteles.
+July 17, 1990. Illustrator and Mexican Designer. Born and raised in a city in the
+middle of the desert. He likes to: learn all kinds of unnecessary things. Hates: The
+phrase "Be an adult, for God´s sake!" He has experience in Web design, UI / UX
+and production for media. In his last job, he worked as an illustration leader for
+Yogome, a national educational video game company. He feels comfortable
+working in a team, and knowing more about the people around him. He is
+passionate when it comes to learning about other cultures, languages ​​and gender
+studies. He loves his job as a part-time professor at a university and his dog
+named Bob.`,
+
+    `<i>"The goal of art is not to represent the external appearance of things, but their
+    inner meaning"</i><br>
+    -Aristotle.
 <br>
 <br>
-México, 1990. Amante de la filosofía del arte. Artista orgullosamente mexicana egresada de la licenciatura de Bellas Artes por parte del San Francisco Art Institute, espacio y tiempo en donde su vida cambio 180º. Al terminar regresó a su tierra natal, la CDMX. Trabajó como voluntaria en el Museo Jumex en donde aprendió técnicas pedagógicas para la enseñanza de arte contemporáneo. En el 2018 fue la directora de Espacio Fidencia, un espacio multicultural para artistas en México. Hoy en día cursa un diplomado en Arte Terapia por medio de la escuela Taller Mexicano de Arteterapia en la Ciudad de México.
-Grandes pasiones: Enseñarle a la gente a explorar su lado creativo y emocional a través de la pintura y diversión. En sus ratos libres ama cocinar comida vegetariana. Actualmente trabaja como Directora de
-Comunicación y Arte para The Pale Blue Dot llevándole a la gente de las comunidades la oportunidad de ser un explorador del mundo mediante prácticas artísticas.`,
-`<i>“Estoy convencida que la educación es nuestra herramienta primordial que tenemos para lograr
-escucharnos y entendernos a nosotros mismos y a los que nos rodean. La educación es un ejercicio de
-cultivo constante; nadie nunca tendría por qué dejar de aprender. Al final, citando al educador brasileño Paulo Freire, la educación per se no cambia al mundo, cambia a las personas que cambian al mundo. Y
-para eso estamos aquí nosotros, ¿no? ”</i>
+Mexico, 1990. Lover of the philosophy of art. Proudly Mexican artist graduated
+from the Bachelor of Fine Arts by the San Francisco Art Institute, space and time
+where her life changed 180º. When she finished she returned to her homeland, the
+CDMX. She worked as a volunteer at the Jumex Museum where she learned
+pedagogical techniques for the teaching of contemporary art. In 2018 she was the
+Director of Espacio Fidencia, a multicultural space for artists in Mexico. Nowadays,
+she is taking a diploma in Art Therapy through the Mexican Workshop School of
+Art Therapy in Mexico City.
+Great passions: Teach people to explore their creative and emotional side through
+painting and fun. In her spare time she loves cooking vegetarian food. She
+currently works as Director of Communication and Art for The Pale Blue Dot, giving
+people of the communities the opportunity to be an explorer of the world through
+artistic practices.`,
+
+`<i>"I am convinced that education is our primary tool that we have to be able to listen
+and understand ourselves and those around us. Education is an exercise in
+constant cultivation; No one would ever have to stop learning. In the end, citing
+Brazilian educator Paulo Freire, education per se does not change the world, it
+changes the people who change the world. And that&#39;s what we&#39;re here for, is not it?
+"</i>
 <br><br>
-Egresada de la licenciatura en Animación y Arte Digital por parte del Tecnológico de Monterrey Campus
-Estado de México. Con un Diplomado en Danza Terapéutica Humanística por parte de Sensodanza A.C., un Diplomado en Antropología del Arte por parte del Laboratorio Transdisciplinario de Investigación y
-Reinvensión, y con el tercer módulo finalizado del Diplomado de Escritura Literaria por parte de Literaria
-Centro Mexicano de Escritores. Desde el 2009 ha impartido diversos talleres a niños de arte, movimiento,
-lectura y escritura. Publicada en la antología de relato corto "Vita Contemplativa: Los invisibles"; en Textofilia
-Ediciones bajo el sello Libros del Marqués; ha participado tanto como autora poeta como moderadora endiferentes presentaciones de poesía. Actualmente escritora en Verde Claro, diseñadora educativa y denarrativa en KurowiGames, Scrum Master con equipos de desarrolladores, y consultora de educación en The Pale Blue Dot. Calculadoramente impulsiva. Con tendencia a ser etiquetada de idealista; nunca aprendió a
-cortarse las alas. Sus herramientas predilectas para saciar su necesidad imperiosa de contarse a sí misma y ocuparse de su persona son la escritura, el cuerpo y la imagen. La primera mujer nómada de raíces fuertes.<br>
-En constante búsqueda de su reflejo en la pupila del Otro.    
+Graduated from the degree in Animation and Digital Art by the Tecnológico de
+Monterrey Campus Estado de México. With a Diploma in Humanistic Therapeutic
+Dance by Sensodanza A.C., a Diploma in Anthropology of Art from the
+Transdisciplinary Research and Reinvestment Laboratory, and with the third
+completed module of the Literary Writing Diploma by Literary Centro Mexicano de
+Escritores. Since 2009 he has given various workshops to children of art,
+movement, reading and writing. Published in the anthology of short story "Vita
+Contemplativa: Los invisibles"; in Textofilia Editions under the seal Libros del
+Marqués; she has participated both as poet author and moderator in different
+presentations of poetry. She is currently a writer in Verde Claro, educational and
+denarrative designer in KurowiGames, Scrum Master with teams of developers,
+and education consultant in The Pale Blue Dot. Calculatingly impulsive. With a
+tendency to be labeled as idealist; he never learned to cut his wings. Her favorite
+tools to satisfy her imperative need to tell herself and take care of herself are
+writing, body and image. The first nomad woman with strong roots.
+In constant search of its reflection in the pupil of the Other.    
     
     `,
-    "Diego es el programador frontend The Pale Blue Dot, nacido en la cuna de la mexicanidad Iztapalapa graduado de la carrera de diseño gráfico, pero se enfocó más en la parte de programación donde encontró su pasión, trabajando por 5 años como desarrollador e-learning haciendo interactivos y cursos en línea dio el salto a juegos en HMTL5 para la empresa Yogome. Junto a un gran equipo realizó aplicaciones en línea como reto cuantrix para Televisa.<br><br>Diego le gustan los juegos de arcade y competir con sus amigos en juegos de pelea, y su otra gran pasión son los autos.",
-    "Ignacio es originario de la Ciudad de México. Orgulloso “Puma” ya que cuenta con una Licenciatura en Contaduría por la Universidad Nacional Autónoma de México. Aparte de sus labores como un especialista fiscal, es también socio accionista de “Cuarto Piso Boutique Audio - Visual” una agencia de medios publicitarios.<br><br> En su tiempo libre goza de la lectura y de la pintura."
+    `Diego is the frontend programmer at The Pale Blue Dot, born in the birthplace of la
+“mexicanidad”, Iztapalapa. Graduated from the graphic design career, but focused
+more on the part of programming where he found his passion. Working for 5 years
+as an e-learning developer doing Interactive and online courses gave the jump to
+games in HMTL5 for the company Yogome. Together with a great team he
+developed online applications for the “Quantrix challenge” for Televisa. Diego likes
+arcade games, and competing with his friends in fight games; and his other great
+passion is cars.`,
+`Ignacio is originally from Mexico City. Proud "Puma" since he has a Bachelor of
+Accounting from the National Autonomous University of Mexico (UNAM). Apart
+
+from his work as a tax specialist, he is also a shareholder of "Cuarto Piso Boutique
+Audio - Visual", an advertising media agency.
+In his free time he enjoys reading and painting.`
 ]
 
 for(var i = 0; i<=photosTeam.length-1;i++){
